353ac59566490c162b7903388650c4d9b3c17f35
Julián Arenas-Guerrero <juliquiag@hotmail.com>
2021-06-21 19:50:36 +0200

    adapting enclosing characters for each RDB engine

--- morph-kgc/data_source/relational_source.py
+++ morph-kgc/data_source/relational_source.py
@@ -35,20 +35,49 @@ SQL_RDF_DATATYPE = {
 }
 
 
-def relational_db_connection(config, source_name):
+def _replace_query_enclosing_characters(sql_query, db_dialect):
+    db_dialect = db_dialect.upper()
+    dialect_sql_query = ''
 
-    return create_engine(config.get_database_url(source_name), poolclass=NullPool)
+    if db_dialect == 'MYSQL' or db_dialect == 'MARIADB':
+        dialect_sql_query = sql_query   # the query already uses backtics as enclosed characters
+    elif db_dialect == 'MSSQL':
+        # replace backticks with square brackets
+        square_brackets = ['[', ']']
+        num_enclosing_char = 0
+        for char in sql_query:
+            if char == '`':
+                dialect_sql_query = dialect_sql_query + square_brackets[num_enclosing_char % 2]
+                num_enclosing_char = num_enclosing_char + 1
+            else:
+                dialect_sql_query = dialect_sql_query + char
+    else:
+        # replace backticks with double quotes
+        dialect_sql_query = sql_query.replace('`', '"')
+
+    return dialect_sql_query
+
+
+
+def relational_db_connection(config, source_name):
+    db_connection = create_engine(config.get_database_url(source_name), poolclass=NullPool)
+    db_dialect = db_connection.dialect.name.upper()
+
+    return db_connection, db_dialect
 
 
 def get_column_datatype(config, source_name, table_name, column_name):
-    db_connection = relational_db_connection(config, source_name)
-    query = "SELECT data_type FROM information_schema.columns WHERE table_name='" + table_name + \
-            "' AND column_name='" + column_name + "' ;"
+    db_connection, db_dialect = relational_db_connection(config, source_name)
+
+    sql_query = "SELECT `data_type` FROM `information_schema`.`columns` WHERE `table_name`='" + table_name + \
+            "' AND `column_name`='" + column_name + "'"
+
+    sql_query = _replace_query_enclosing_characters(sql_query, db_dialect)
 
     try:
-        query_results_df = pd.read_sql(query, con=db_connection)
+        query_results_df = pd.read_sql(sql_query, con=db_connection)
     except:
-        raise Exception('Query [' + query + '] has failed to execute.')
+        raise Exception('Query [' + sql_query + '] has failed to execute.')
 
     data_type = ''
     if 'data_type' in query_results_df.columns and len(query_results_df) == 1:
@@ -59,7 +88,7 @@ def get_column_datatype(config, source_name, table_name, column_name):
     if data_type.upper() in SQL_RDF_DATATYPE:
         return SQL_RDF_DATATYPE[data_type.upper()]
     else:
-        return ''
+        return 'http://www.w3.org/2001/XMLSchema#string'
 
 
 def build_sql_join_query(mapping_rule, parent_triples_map_rule, references, parent_references):
@@ -71,11 +100,11 @@ def build_sql_join_query(mapping_rule, parent_triples_map_rule, references, pare
     child_query = build_sql_subquery(mapping_rule, references, 'child_')
     parent_query = build_sql_subquery(parent_triples_map_rule, parent_references, 'parent_')
 
-    query = 'SELECT * FROM (' + child_query + ') AS child, (' + parent_query + ') AS parent WHERE '
+    query = 'SELECT * FROM (' + child_query + ') AS `child`, (' + parent_query + ') AS `parent` WHERE '
     for key, join_condition in eval(mapping_rule['join_conditions']).items():
-        query = query + 'child.child_' + join_condition['child_value'] + \
-                '=parent.parent_' + join_condition['parent_value'] + ' AND '
-    query = query[:-4] + ';'
+        query = query + '`child`.`child_' + join_condition['child_value'] + \
+                '`=`parent`.`parent_' + join_condition['parent_value'] + '` AND '
+    query = query[:-5]
 
     logging.debug('SQL query for mapping rule `' + str(mapping_rule['id']) + '`: [' + query + ']')
 
@@ -89,10 +118,10 @@ def build_sql_subquery(mapping_rule, references, alias):
         query = 'SELECT '
         if len(references) > 0:
             for reference in references:
-                query = query + reference + ' AS ' + alias + reference + ', '
-            query = query[:-2] + ' FROM ' + mapping_rule['tablename'] + ' WHERE '
+                query = query + '`' + reference + '` AS `' + alias + reference + '`, '
+            query = query[:-2] + ' FROM `' + mapping_rule['tablename'] + '` WHERE '
             for reference in references:
-                query = query + reference + ' IS NOT NULL AND '
+                query = query + '`' + reference + '` IS NOT NULL AND '
             query = query[:-5]
     else:
         query = None
@@ -101,6 +130,11 @@ def build_sql_subquery(mapping_rule, references, alias):
 
 
 def build_sql_query(config, mapping_rule, references):
+    """
+    Build a query for MYSQL using backticks '`' as enclosing character. This character will later be replaced with the
+    one corresponding one to the dialect that applies.
+    """
+
     if pd.notna(mapping_rule['query']):
         query = mapping_rule['query']
     elif len(references) > 0:
@@ -108,11 +142,11 @@ def build_sql_query(config, mapping_rule, references):
         if config.push_down_sql_distincts():
             query = query + 'DISTINCT '
         for reference in references:
-            query = query + reference + ', '
-        query = query[:-2] + ' FROM ' + mapping_rule['tablename'] + ' WHERE '
+            query = query + '`' + reference + '`, '
+        query = query[:-2] + ' FROM `' + mapping_rule['tablename'] + '` WHERE '
         for reference in references:
-            query = query + reference + ' IS NOT NULL AND '
-        query = query[:-4] + ';'
+            query = query + '`' + reference + '` IS NOT NULL AND '
+        query = query[:-5]
     else:
         query = None
 
@@ -123,11 +157,15 @@ def build_sql_query(config, mapping_rule, references):
 
 
 def get_sql_data(config, mapping_rule, references, parent_triples_map_rule=None, parent_references=None):
+    db_connection, db_dialect = relational_db_connection(config, mapping_rule['source_name'])
+
     if parent_triples_map_rule is not None:
         sql_query = build_sql_join_query(mapping_rule, parent_triples_map_rule, references, parent_references)
     else:
         sql_query = build_sql_query(config, mapping_rule, references)
-    db_connection = relational_db_connection(config, mapping_rule['source_name'])
+
+    sql_query = _replace_query_enclosing_characters(sql_query, db_dialect)
+
     result_chunks = pd.read_sql(sql_query,
                                 con=db_connection,
                                 chunksize=config.get_chunksize(),
