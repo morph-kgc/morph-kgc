952ebb698d13925dd98776403e26f9f012215f8d
Julián Arenas-Guerrero <juliquiag@hotmail.com>
2022-10-11 10:13:59 +0200

    move test case for issue #62

--- test/issues/issue_62/test_issue_62.py
+++ test/issues/issue_62/test_issue_62.py
@@ -13,7 +13,7 @@ from rdflib.graph import Graph
 from rdflib import compare
 
 
-def test_doubleJoinCondition():
+def test_issue_62():
     g = Graph()
     g.parse(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output.nq'))
 
