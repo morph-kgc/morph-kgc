7d7fadb1ff1033f10f07e9cd5d1275fee5ccf88f
Julián Arenas-Guerrero <arenas.guerrero.julian2outlook.com>
2022-11-19 13:26:50 +0100

    refactoring and removing subject_map and object_map columns when no longer needed

--- src/morph_kgc/mapping/mapping_parser.py
+++ src/morph_kgc/mapping/mapping_parser.py
@@ -263,7 +263,6 @@ def _transform_mappings_into_dataframe(mapping_graph, section_name):
     # mapping rules in graph to DataFrame
     source_mappings_df = pd.DataFrame(mapping_query_results.bindings)
     source_mappings_df.columns = source_mappings_df.columns.map(str)
-    source_mappings_df = source_mappings_df.reindex(columns=MAPPINGS_DATAFRAME_COLUMNS)
 
     # process mapping rules with joins
     # create a dict with child triples maps in the keys and its join conditions in the values
@@ -279,8 +278,6 @@ def _transform_mappings_into_dataframe(mapping_graph, section_name):
     # convert the join condition dicts to string (can later be converted back to dict)
     source_mappings_df['object_join_conditions'] = source_mappings_df['object_join_conditions'].astype(str)
     source_mappings_df['subject_join_conditions'] = source_mappings_df['subject_join_conditions'].astype(str)
-    # object_map and subject_map columns no longer needed, remove them
-    source_mappings_df = source_mappings_df.drop(['subject_map', 'object_map'], axis=1)
 
     # convert all values to string
     for i, row in source_mappings_df.iterrows():
@@ -409,6 +406,8 @@ class MappingParser:
                 self.mappings_df = pd.concat([self.mappings_df, data_source_mappings_df])
 
         self.mappings_df = self.mappings_df.reset_index(drop=True)
+        # subject_map and object_map columns were used to handle join conditions, no longer needed
+        self.mappings_df = self.mappings_df.drop(columns=['subject_map', 'object_map'])
 
     def _parse_data_source_mapping_files(self, section_name):
         """
@@ -656,41 +655,3 @@ class MappingParser:
                         self.mappings_df.at[i, 'object_termtype'] = parent_triples_map_rule.at['subject_termtype']
                         self.mappings_df.at[i, 'object_join_conditions'] = np.nan
                         logging.debug(f"Removed self-join from mapping rule `{mapping_rule['id']}`.")
-
-    # # TODO: deprecate
-    # def _remove_self_joins_from_mappings(self):
-    #     # because RDB has no rml:source, use source_name for rml:source to facilitate self-join elimination
-    #     self.mappings_df.loc[self.mappings_df['source_type'] == RDB, 'data_source'] = self.mappings_df['source_name']
-    #
-    #     for i, mapping_rule in self.mappings_df.iterrows():
-    #         if pd.notna(mapping_rule['object_parent_triples_map']):
-    #             parent_triples_map_rule = get_mapping_rule(self.mappings_df, mapping_rule['object_parent_triples_map'])
-    #
-    #             # str() is to be able to compare np.nan
-    #             if str(mapping_rule['data_source']) == str(parent_triples_map_rule['data_source']) and \
-    #                     str(mapping_rule['tablename']) == str(parent_triples_map_rule['tablename']) and \
-    #                     str(mapping_rule['iterator']) == str(parent_triples_map_rule['iterator']) and \
-    #                     str(mapping_rule['query']) == str(parent_triples_map_rule['query']):
-    #
-    #                 remove_join = True
-    #                 # check that all conditions in the join condition have the same references
-    #                 try:
-    #                     join_conditions = eval(mapping_rule['object_join_conditions'])
-    #                     for key, join_condition in join_conditions.items():
-    #                         if join_condition['child_value'] != join_condition['parent_value']:
-    #                             remove_join = False
-    #                 except:
-    #                     # eval() has failed because the are no join conditions, the join can be removed
-    #                     remove_join = True
-    #
-    #                 if remove_join:
-    #                     self.mappings_df.at[i, 'object_parent_triples_map'] = ''
-    #                     self.mappings_df.at[i, 'object_join_conditions'] = ''
-    #
-    #                     if self.mappings_df.at[i, 'object_map_type'] == parent_triples_map_rule.at['subject_map_type']:
-    #                     # self.mappings_df.at[i, 'object_constant'] = parent_triples_map_rule.at['subject_constant']
-    #                     # self.mappings_df.at[i, 'object_template'] = parent_triples_map_rule.at['subject_template']
-    #                     # self.mappings_df.at[i, 'object_reference'] = parent_triples_map_rule.at['subject_reference']
-    #                     # self.mappings_df.at[i, 'object_termtype'] = parent_triples_map_rule.at['subject_termtype']
-    #                         self.mappings_df.at[i, 'object_map_value'] = parent_triples_map_rule.at['subject_map_value']
-    #                         logging.debug(f"Removed self-join from mapping rule `{mapping_rule['id']}`.")
