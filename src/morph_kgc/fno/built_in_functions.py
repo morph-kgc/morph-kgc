a4df1109f115779eb08067bed86d3d91a7e7a959
Julián Arenas-Guerrero <arenas.guerrero.julian2outlook.com>
2022-12-22 16:31:17 +0100

    controls_if and constrols_if_cast functions

--- src/morph_kgc/fno/built_in_functions.py
+++ src/morph_kgc/fno/built_in_functions.py
@@ -118,9 +118,34 @@ def to_upper_case(string):
     return string.upper()
 
 
+@bif(
+    fun_id='http://users.ugent.be/~bjdmeest/function/grel.ttl#controls_if',
+    boolean_expression='http://users.ugent.be/~bjdmeest/function/grel.ttl#bool_b',
+    value_true='http://users.ugent.be/~bjdmeest/function/grel.ttl#any_true',
+    value_false='http://users.ugent.be/~bjdmeest/function/grel.ttl#any_false')
+def controls_if(boolean_expression, value_true, value_false=None):
+    if eval(boolean_expression):
+        return value_true
+    else:
+        return value_false
+
+
 ##############################################################################
 ########################   OTHER   ###########################################
 ##############################################################################
+
+
+@bif(
+    fun_id='https://github.com/oeg-upm/morph-kgc/function/built-in.ttl#controls_if_cast',
+    string='http://users.ugent.be/~bjdmeest/function/grel.ttl#bool_b',
+    value_true='http://users.ugent.be/~bjdmeest/function/grel.ttl#any_true',
+    value_false='http://users.ugent.be/~bjdmeest/function/grel.ttl#any_false')
+def controls_if_cast(string, value_true, value_false=None):
+    if string.lower() in ['', 'false', 'no', 'off', '0']:
+        # this will be filtered when removing nulls
+        return value_false
+    else:
+        return value_true
 
 
 @bif(
