d6196fd81f10f45eb92436c981349279c1f9a0d4
Julián Arenas Guerrero <arenas.guerrero.julian@outlook.com>
2023-02-03 18:32:40 +0100

    ttl file extension

--- test/issues/issue_145/test_issue_145.py
+++ test/issues/issue_145/test_issue_145.py
@@ -17,7 +17,7 @@ def test_issue_145():
     g = Graph()
     g.parse(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output.nq'))
 
-    mapping_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.rml')
+    mapping_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.ttl')
     config = f'[CONFIGURATION]\noutput_format=N-QUADS\n[DataSource]\nmappings={mapping_path}'
     g_morph = morph_kgc.materialize(config)
 
