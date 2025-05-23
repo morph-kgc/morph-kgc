23703de487a8d14f6307f9908fb6931b0c0f2329
Julián Arenas Guerrero <arenas.guerrero.julian@outlook.com>
2025-05-23 14:27:41 +0200

    cleaning

--- test/rml-fnml/other_functions/type/test_other_type.py
+++ test/rml-fnml/other_functions/type/test_other_type.py
@@ -11,9 +11,9 @@ import rdflib
 import rdflib.compare
 import morph_kgc
 
-def test_other_type():    
+def test_other_type():
     mapping_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.yarrrml')
-    csv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'cars.csv')
-    config = f'[DataSource]\nmappings:{mapping_path}\nfile_path:{csv_path}'
+    csv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    config = f'[DataSource]\nmappings:{mapping_path}\n'
     rml_morph = morph_kgc.materialize(config)
     assert all([o.value == "str" for s,v,o in rml_morph])
