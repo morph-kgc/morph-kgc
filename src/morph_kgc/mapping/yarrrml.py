6f0e64d35e23755737c2af11330cbd327ff24606
Achim Reiz <achim.reiz@neonto.de>
2025-06-01 14:41:32 +0200

    fix(#328): Update RDFa mappings file path to use os.path

--- src/morph_kgc/mapping/yarrrml.py
+++ src/morph_kgc/mapping/yarrrml.py
@@ -8,6 +8,7 @@ __email__ = "arenas.guerrero.julian@outlook.com"
 
 import rdflib
 import json
+import os
 from ruamel.yaml import YAML
 from copy import deepcopy
 from random import randint
@@ -128,7 +129,7 @@ def _add_default_prefixes(mappings):
         'xsd': XSD_NAMESPACE,
         'rdfs': RDFS_NAMESPACE
     }
-    with open("src/morph_kgc/mapping/rdfa_mappings.json", "r") as json_file:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "rdfa_mappings.json"), "r") as json_file:
         rdfa_prefixes = json.load(json_file)
     default_prefixes.update(rdfa_prefixes)
     if 'prefixes' in mappings:
