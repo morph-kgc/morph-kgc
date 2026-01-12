50e5ea63b3ff6f63cf3c1408bd7fa9bbcdca88f9
Julián Arenas-Guerrero <arenas.guerrero.julian@outlook.com>
2026-01-12 11:47:29 +0100

    undo

--- src/morph_kgc/mapping/mapping_parser.py
+++ src/morph_kgc/mapping/mapping_parser.py
@@ -483,34 +483,6 @@ def _validate_termtypes(mapping_graph):
         raise ValueError(f'Found an invalid object termtype. Found values {object_termtypes}. Object maps must be '
                          f'{RML_IRI}, {RML_BLANK_NODE}, {RML_LITERAL} or {RML_RDF_STAR_TRIPLE}.')
 
-def translate_mappings_to_rml(config, source_name=None):
-    """
-    Translates YARRRML / R2RML mappings into normalized RML.
-
-    Parameters
-    ----------
-    config : Config | str
-        Morph-KGC configuration object or path to config file.
-    source_name : str, optional
-        If provided, only translates mappings of that data source.
-
-    Returns
-    -------
-    dict
-        Dictionary mapping source_name -> rdflib.Graph (RML)
-    """
-
-    parser = MappingParser(config)
-    rml_graphs = {}
-
-    for section in config.get_data_sources_sections():
-        if source_name and section != source_name:
-            continue
-
-        rml_graphs[section] = parser.parse_and_normalize_to_rml_graph(section)
-
-    return rml_graphs
-
 
 class MappingParser:
 
@@ -546,25 +518,6 @@ class MappingParser:
         self.rml_df = mapping_partitioner.partition_mappings()
 
         return self.rml_df, self.fnml_df, self.http_api_df
-
-    def parse_and_normalize_to_rml_graph(self, section_name):
-        """
-        Loads mappings (YARRRML / R2RML / RML),
-        normalizes them to standard RML,
-        validates them, and returns an RDFLib Graph.
-
-        This method does NOT materialize data.
-        """
-        # Load original mapping graph
-        mapping_graph = self._load_mapping_graph(section_name)
-
-        # Normalize (R2RML → RML, legacy RML → RML, shortcuts, etc.)
-        mapping_graph = self._normalize_mapping_graph(mapping_graph)
-
-        # Complete triples map classes and validate term types
-        mapping_graph = self._complete_and_validate_mapping(mapping_graph)
-
-        return mapping_graph
 
     def _get_from_r2_rml(self):
         """
@@ -688,13 +641,14 @@ class MappingParser:
         """
 
         for i, rml_rule in self.rml_df.iterrows():
-            if pd.notna(rml_rule['reference_formulation']) and 'SQL' in rml_rule['reference_formulation'].upper():
-                self.rml_df.at[i, 'source_type'] = RDB
-            elif pd.notna(rml_rule['reference_formulation']) and 'CYPHER' in rml_rule['reference_formulation'].upper():
-                self.rml_df.at[i, 'source_type'] = PGDB
-            elif self.config.has_db_url(rml_rule['source_name']):
-                # if db_url but no reference formulation, assume it is a relational database
-                self.rml_df.at[i, 'source_type'] = RDB
+            if self.config.has_db_url(rml_rule['source_name']):
+                if pd.notna(rml_rule['reference_formulation']) and 'SQL' in rml_rule['reference_formulation'].upper():
+                    self.rml_df.at[i, 'source_type'] = RDB
+                elif pd.notna(rml_rule['reference_formulation']) and 'CYPHER' in rml_rule['reference_formulation'].upper():
+                    self.rml_df.at[i, 'source_type'] = PGDB
+                else:
+                    # if db_url but no reference formulation, assume it is a relational database
+                    self.rml_df.at[i, 'source_type'] = RDB
             elif rml_rule['logical_source_type'] == RML_QUERY:
                 # it is a query, but it is not a DB (because no db_url), hence it is a tabular view
                 # assign CSV (it can also be Apache Parquet but format is automatically inferred)
@@ -707,7 +661,10 @@ class MappingParser:
             elif rml_rule['logical_source_type'] == RML_SOURCE:
                 # it is a file, infer source type from file extension
                 file_extension = os.path.splitext(str(rml_rule['logical_source_value']))[1][1:].strip()
-                if file_extension.upper() in FILE_SOURCE_TYPES:
+
+                if pd.notna(rml_rule['reference_formulation']) and GEOPARQUET in rml_rule['reference_formulation'].upper():
+                    self.rml_df.at[i, 'source_type'] = GEOPARQUET
+                elif file_extension.upper() in FILE_SOURCE_TYPES:
                     self.rml_df.at[i, 'source_type'] = file_extension.upper()
                 elif pd.notna(rml_rule['reference_formulation']):
                     # if file extension is not recognized, use reference formulation
@@ -931,4 +888,3 @@ class MappingParser:
                         self.rml_df.at[i, 'object_termtype'] = parent_triples_map_rule.at['subject_termtype']
                         self.rml_df.at[i, 'object_join_conditions'] = None
                         LOGGER.debug(f"Removed self-join from mapping rule `{rml_rule['triples_map_id']}`.")
-
