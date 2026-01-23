c1fc7dc256e162de1319c926ffb05ee963f6f925
iz4ack <luciaisaac.nv@gmail.com>
2026-01-23 14:57:46 +0100

    Fix http_api filters match with key when reference doesn't exist

--- src/morph_kgc/data_source/http_api.py
+++ src/morph_kgc/data_source/http_api.py
@@ -6,26 +6,13 @@ __maintainer__ = "Julián Arenas-Guerrero"
 __email__ = "arenas.guerrero.julian@outlook.com"
 
 
+import json
+import urllib.request
 import pandas as pd
-import os
-import importlib.util
-import sys
 
-from pathlib import Path
 from jsonpath import JSONPath
 from io import StringIO
 from ..utils import normalize_hierarchical_data
-
-
-def load_module_from_path(module_name, file_path):
-    file_path = str(Path(file_path).resolve())
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    if spec is None or spec.loader is None:
-        raise ImportError(f"Cannot load spec for {module_name} from {file_path}")
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    sys.modules[module_name] = module  # optional: register for reuse
-    return module
 
 
 def get_http_api_data(config, rml_rule, references):
@@ -38,16 +25,10 @@ def get_http_api_data(config, rml_rule, references):
     headers = {}
     if 'field_name' in df.columns:
         for i, row in df.iterrows():
-            if row['field_name'] in os.environ:
-                field_value = row['field_value'].format(**os.environ)
-            else:
-                mod = load_module_from_path("dynamic_api_token", config.get_api_token())
-                field_value = mod.get_api_token(arg1=row['field_value'])
-
             if row['field_name'].lower() in ['authorization', 'accept', 'keyid', 'user-agent']:
-                headers[row['field_name']] = field_value
+                headers[row['field_name']] = row['field_value']
             else:
-                payload[row['field_name']] = field_value
+                payload[row['field_name']] = row['field_value']
     json_data = requests.get(absolute_path, params=payload, headers=headers).json()
 
     # Check if any of the references have a JSONPath filter; it's treated differently.
@@ -70,23 +51,30 @@ def get_http_api_data(config, rml_rule, references):
     # normalize and remove nulls
     json_df = pd.json_normalize([json_object for json_object in normalize_hierarchical_data(jsonpath_result) if
                                  None not in json_object.values()])
+    
     if filter_refs:
-        # Given a filter reference like "MetaData[?(@.Variable.Id==357)].Nombre" we need to convert it to "MetaData[?(@.Variable.Id==357)].(Nombre)"
-        last_field = filter_refs[0][filter_refs[0].rfind('.') + 1:] 
-        filter = filter_refs[0][:filter_refs[0].rfind('.') + 1] + f"({last_field})"
+            join_key = simple_refs[0] 
+            entries = JSONPath("$.*").parse(json_data)
+            lookup_data = {item.get(join_key): item for item in entries if item.get(join_key)}
 
-        # same as above but for filtered references
-        jsonpath_result_filters = JSONPath(rml_rule['iterator'] + "." + filter).parse(json_data)
-        flat_filters = pd.json_normalize([json_object for json_object in normalize_hierarchical_data(jsonpath_result_filters) if
-                                 None not in json_object.values()])
-        
-        # add the filtered column to the main dataframe
-        json_df[filter_refs[0]] = flat_filters
+            for filter_ref in filter_refs:
+                column_value = []
+                for key_value in json_df[join_key]:
+                    match = lookup_data.get(key_value)
+                    if match:
+                        res = JSONPath(f"$..{filter_ref}").parse(match)
+                        column_value.append(res[0] if res else None)
+                    else:
+                        column_value.append(None)
+                json_df[filter_ref] = column_value
 
     # add columns with null values for those references in the mapping rule that are not present in the data file
     missing_references_in_df = list(set(references).difference(set(json_df.columns)))
     json_df[missing_references_in_df] = None
     #json_df.dropna(axis=1, how='any', inplace=True) #This removes everything if threres a null; it should only keep the columns that are in the reference and remove the rest.
+    # Drop rows with None values in some columns
+    json_df = json_df.dropna(axis=0, how='any', subset=[c for c in references if c in json_df.columns])
+    #
     json_df = json_df[[c for c in references if c in json_df.columns]] #Take only the columns that are in the references.
 
     return json_df
